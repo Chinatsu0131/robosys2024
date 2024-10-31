1025d45abca6e9da4449f640b7e85b0cd196877f
Chinatsu0131 <xiaolinq509@gmail.com>
2024-10-31 12:36:54 +0900

    add a comand

--- test.bash
+++ test.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2024 Takumi Kobayashi
+# SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
         echo ${1}行目が違うよ
@@ -6,8 +8,10 @@ ng () {
 }
 
 res=0
-a=すいちゃんは今日も小さい
-[ "$a" = すいちゃんは今日も可愛い ] || ng "$LINENO"
-[ "$a" = すいちゃんは今日も小さい ] || ng "$LINENO"
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
 
 exit "$res"
